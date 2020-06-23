742a53f7fd05c3f95f5f07602be3b152d6b8ccdc
Juliano Atanazio <juliano777@gmail.com>
2020-06-23 09:44:56 -0300

    unpack

--- anotacoes.py
+++ anotacoes.py
@@ -27,4 +27,59 @@ Curitiba - PR: Sul
 Natal - RN: Nordeste
 Campo Grande - MS: Centro Oeste
 Manaus - AM: Norte
-'''    
+''' 
+
+
+# Unpack values ==============================================================
+
+x, y = (7, 9)
+
+print(x)
+7
+
+print(y)
+9
+
+# Caso queira descartar algum valor
+x, _ = (7, 9)
+
+print(x)
+7
+
+
+
+x, y, *z = (1, 2, 3, 4, 5)
+
+print(x)                                                                                                                                                                                                   
+1
+
+print(y)                                                                                                                                                                                                   
+2
+
+print(z)                                                                                                                                                                                                   
+[3, 4, 5]
+
+
+
+x, *y, z = (1, 2, 3, 4, 5)
+
+print(x)                                                                                                                                                                                                   
+1
+
+print(y)                                                                                                                                                                                                  
+[2, 3, 4]
+
+print(z)                                                                                                                                                                                                  
+5
+
+
+*x, y, z = (1, 2, 3, 4, 5)                                                                                                                                                                                
+
+print(x)                                                                                                                                                                                                  
+[1, 2, 3]
+
+print(y)                                                                                                                                                                                                  
+4
+
+print(z)                                                                                                                                                                                                  
+5
